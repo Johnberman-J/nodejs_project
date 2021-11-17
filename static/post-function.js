80bbf16234bb6357fb48a99746d877b7f6e0fcf3
Jason Lee <babo318@gmail.com>
2021-11-18 02:00:05 +0900

    post function making ing

---
+++ static/post-function.js
@@ -0,0 +1,10 @@
+function moveIndexpage() {
+    const userName = $('#userName').val();
+    const title = $('#title').val();
+    const password = $('#password').val();
+    const content = $('#content').val();
+
+    console.log(userName,title,password,content)
+
+    location.href='index.html'
+}
